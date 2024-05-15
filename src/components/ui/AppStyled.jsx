a548b07930351fe6ebd7c3d8dab2e72e2874c86e
Ragnarok794 <adrianizq94@gmail.com>
2024-05-15 19:32:26 +0200

    Changing padding and with in appstyled for better netlify deploy

--- src/components/ui/AppStyled.jsx
+++ src/components/ui/AppStyled.jsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const AppStyled = styled.div`
     margin: 0;
     padding: 0 10px;
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     
 `
